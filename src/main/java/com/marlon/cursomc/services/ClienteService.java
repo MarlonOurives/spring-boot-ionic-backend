bbd23076e09ffb92105617e23bede12e26f7a094
Marlon <marloncicero_araujo@hotmail.com>
2019-05-21 07:42:32 -0400

    Endpoint  /clientes/{id} disponivel

---
+++ src/main/java/com/marlon/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.marlon.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marlon.cursomc.domain.Cliente;
+import com.marlon.cursomc.repositories.ClienteRepository;
+import com.marlon.cursomc.services.exceptions.ObjectNotFoundException;
+
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+	//===Realiza Busca na categoria pelo Id===// 
+	@Autowired
+	private ClienteRepository repo;
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+		
+	}
+}
