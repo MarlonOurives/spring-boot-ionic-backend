2e340eb706c68f412834d812d92542ae05358d04
Marlon <marloncicero_araujo@hotmail.com>
2019-05-30 18:52:50 -0400

    Utilizando o DELETE em uma categoria

--- src/main/java/com/marlon/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/marlon/cursomc/resources/CategoriaResource.java
@@ -42,4 +42,11 @@ public class CategoriaResource {
 		return ResponseEntity.noContent().build();
 		
 	}
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.DELETE)
+	//=== O PathVariable vai avisar que o id vai vim pelo get ===//
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/marlon/cursomc/services/CategoriaService.java
+++ src/main/java/com/marlon/cursomc/services/CategoriaService.java
@@ -1,10 +1,12 @@
 package com.marlon.cursomc.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.marlon.cursomc.domain.Categoria;
 import com.marlon.cursomc.repositories.CategoriaRepository;
+import com.marlon.cursomc.services.exceptions.DataIntegrityException;
 import com.marlon.cursomc.services.exceptions.ObjectNotFoundException;
 
 import java.util.Optional;
@@ -29,5 +31,14 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 }
